b5433badd5642dc2255e66b7ce153c9a3e81a7d6
Lukas Jakob <lu.jakob@googlemail.com>
2017-12-18 11:17:35 +0100

    Refactor file structure

---
+++ webpack.client.config.js
@@ -0,0 +1,45 @@
+const path = require('path');
+const ExtractTextPlugin = require("extract-text-webpack-plugin")
+
+
+module.exports = {
+  devtool: 'source-map',
+  entry: './client/src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'client/dist')
+  },
+  devServer: {
+    contentBase: './client/public',
+    publicPath: '/dist/'
+  },
+  module: {
+    loaders: [
+      // ES6 enabled
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        exclude: /(node_modules)/
+      },
+      {
+        test: /\.scss$/,
+        use: ExtractTextPlugin.extract({
+          fallback: "style-loader",
+          use: [{
+            loader: "css-loader", options: {
+              sourceMap: true
+            }
+          }, {
+            loader: "sass-loader", options: {
+              sourceMap: true
+            }
+          }],
+          publicPath: ""
+        })
+      }
+    ]
+  },
+  plugins: [
+    new ExtractTextPlugin('main.css'),
+  ]
+};
